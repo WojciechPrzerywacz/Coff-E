46cb9d5e02f25e95479dbe9542afb3ec7f98e723
Dawid <sgace2t1@gmail.com>
2021-01-04 18:34:24 +0100

    Dodano ekranik hehe

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,12 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
 
 import javax.imageio.ImageIO;
@@ -15,6 +17,8 @@ import java.io.IOException;
 public class Controller {
 
     final public double MIN_ROTATE_VALUE = 0, MAX_ROTATE_VALUE = 360;
+    public ScrollPane screenScrollPane;
+    public ImageView screenFirstLayer;
     double xOffset, yOffset;
     public AnchorPane backgroundAnchorPane;
 
@@ -24,23 +28,27 @@ public class Controller {
     public ImageView textImage;
     public ImageView knobLight;
 
-    public int flag=0;
+    public int flag = 0;
 
     @FXML
     private void handleButton(ActionEvent event) {
-
-        if (event.getSource().equals(powerButton)){
-            if(flag==0) {
+        if (event.getSource().equals(powerButton)) {
+            if (flag == 0) {
                 textImage.setImage(new Image(getClass().getResourceAsStream("assets/brand-logo-on.png")));
                 knobLight.setImage(new Image(getClass().getResourceAsStream("assets/volume-indicator-on.png")));
+                screenScrollPane.setVisible(true);
                 flag = 1;
-            }else {
+            } else {
                 textImage.setImage(new Image(getClass().getResourceAsStream("assets/brand-logo-off.png")));
                 knobLight.setImage(new Image(getClass().getResourceAsStream("assets/volume-indicator-off.png")));
+                screenScrollPane.setVisible(false);
                 flag = 0;
             }
         }
 
+        // Sztuczne testowe przesuwanie ekraniku fiku miku
+        // screenScrollPane.setVvalue(screenScrollPane.getVvalue() + screenScrollPane.getVmax() / 3);
+        // System.out.println(screenScrollPane.getVvalue());
     }
 
     public void setRotate(MouseEvent event) {
@@ -62,5 +70,9 @@ public class Controller {
     public void moveWindow(MouseEvent event) {
         // backgroundAnchorPane.getScene().getWindow().setX(event.getScreenX() + xOffset);
         // backgroundAnchorPane.getScene().getWindow().setY(event.getScreenY() + yOffset);
+    }
+
+    public void doNotScroll(ScrollEvent event) {
+        event.consume();
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ import javafx.stage.StageStyle;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         Scene scene = new Scene(root, 683, 683);
